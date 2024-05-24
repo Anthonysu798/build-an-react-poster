4ad4936598d3cd01da55029b619ec258b25a42af
Anthony Su <anthonysu798.ca@gmail.com>
2024-05-24 00:21:09 -0400

    useState

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,18 +1,28 @@
-import classes from './NewPost.module.css';
+import { useState } from 'react';
+import classes from "./NewPost.module.css";
 
 function NewPost() {
-  return (
-    <form className={classes.form}>
-      <p>
+    const [enteredBody, setEnteredBody] = useState('');
+
+    function changeBodyHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+    
+    return (
+        <form className={classes.form}>
+        <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
-      </p>
-      <p>
+        <textarea id="body" required rows={3} onChange={changeBodyHandler}/> 
+        </p>
+        
+        <p>{enteredBody}</p>
+        
+        <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
-      </p>
-    </form>
-  );
+        </p>
+        </form>
+    );
 }
 
 export default NewPost;
