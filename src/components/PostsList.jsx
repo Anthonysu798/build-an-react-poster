1d16a5cfa15b8d347b0ae3324ce93e9bce644f7a
Anthony Su <anthonysu798.ca@gmail.com>
2024-05-23 23:50:40 -0400

    Added NewPost Form

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,9 +1,11 @@
 import Post from '@/src/components/Post.jsx';
 import classess from '@/src/components/PostsList.module.css';
+import NewPost from '@/src/components/NewPost.jsx';
 
 function PostsList() {
     return (
         <>
+            <NewPost />
             <ul className={classess.posts}>
             <Post author="Anthony" body="React.js is awesome!" />
             <Post author="Manuel" body="Check out the full course!" />
