64f90a0b781e9845b88a90b21cf10566c24c0e25
Anthony Su <anthonysu798.ca@gmail.com>
2024-05-25 23:50:09 -0400

    Added props

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,18 +1,16 @@
-import { useState } from 'react';
 import classes from "./NewPost.module.css";
 
-function NewPost() {
-    const [enteredBody, setEnteredBody] = useState('');
+function NewPost(props) {
 
     function changeBodyHandler(event) {
         setEnteredBody(event.target.value);
     }
-    
+
     return (
         <form className={classes.form}>
         <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler}/> 
+        <textarea id="body" required rows={3} onChange={props.onBodyChange}/> 
         </p>
         
         <p>{enteredBody}</p>
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,17 @@
+import { useState } from 'react';
+
 import Post from '@/src/components/Post.jsx';
 import classess from '@/src/components/PostsList.module.css';
 import NewPost from '@/src/components/NewPost.jsx';
 
 function PostsList() {
+    const [enteredBody, setEnteredBody] = useState('');
+
     return (
         <>
             <NewPost />
             <ul className={classess.posts}>
-            <Post author="Anthony" body="React.js is awesome!" />
+            <Post author="Anthony" body={enteredBody} />
             <Post author="Manuel" body="Check out the full course!" />
             </ul>
         </>
