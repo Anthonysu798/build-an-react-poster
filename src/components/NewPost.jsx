7ae1f87a577cf8347e172368eb75a3d454ab33f3
Anthony Su <anthonysu798.ca@gmail.com>
2024-05-26 02:31:19 -0400

    Handling Form Submission

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,21 +1,63 @@
+import { useState } from "react";
 import classes from "./NewPost.module.css";
 
-function NewPost(props) {
-    return (
-        <form className={classes.form}>
-        <p>
-        <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={props.onBodyChange}/> 
-        </p>
+function NewPost({ onCancel }) {
+	const [enteredBody, setEnteredBody] = useState("");
+	const [enteredAuthor, setEnteredAuthor] = useState("");
+
+	function bodyChangeHandler(event) {
+		setEnteredBody(event.target.value);
+	}
+
+	function authorChangeHandler(event) {
+		setEnteredAuthor(event.target.value);
+	}
+
+	// Submit handler
+	function submitHandler(event) {
+		// Prevent the user send the form
+		event.preventDefault();
+		const postDate = {
+			body: enteredBody,
+			author: enteredAuthor,
+		};
+        onCancel();
         
-       
-        
-        <p>
-        <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={props.onAuthorChange} />
-        </p>
-        </form>
-    );
+	}
+
+	return (
+		<form className={classes.form} onSubmit={submitHandler}>
+			<p>
+				<label htmlFor="body">Text</label>
+				<textarea
+					id="body"
+					required
+					rows={3}
+					onChange={bodyChangeHandler}
+				/>
+			</p>
+			<p>
+				<label htmlFor="name">Your name</label>
+				<input
+					type="text"
+					id="name"
+					required
+					onChange={authorChangeHandler}
+				/>
+			</p>
+
+			<p className={`${classes.actions} mt-5`}>
+				{" "}
+				{/* Multiple Styling */}
+				{/* Cancel the form when clicked */}
+				<button type="button" onClick={onCancel}>
+					Cancel
+				</button>
+				{/* Only the submit button will submit the form */}
+				<button>Submit</button>
+			</p>
+		</form>
+	);
 }
 
 export default NewPost;
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -7,42 +7,17 @@ import Modal from "@/src/components/Modal.jsx";
 
 function PostsList({isPosting, onStopPosting}) {
 	
-	const [enteredBody, setEnteredBody] = useState("");
-	const [enteredAuthor, setEnteredAuthor] = useState("");
-
-
-	function bodyChangeHandler(event) {
-		setEnteredBody(event.target.value);
-	}
-
-	function authorChangeHandler(event) {
-		setEnteredAuthor(event.target.value);
-	}
-
-	// let modalContent;
-
-	// if (modalIsVisible) {
-	//     modalContent = <Modal onClose={hideModalHandler} >
-	//     <NewPost
-	//         onBodyChange={bodyChangeHandler}
-	//         onAuthorChange={authorChangeHandler}
-	//     />
-	//     </Modal>
-	// }
-
 	return (
 		<>
 			{/* If modalIsVisible is true, show the Modal component */}
 			{isPosting && (
 				<Modal onClose={onStopPosting}>
 					<NewPost
-						onBodyChange={bodyChangeHandler}
-						onAuthorChange={authorChangeHandler}
+                        onCancel={onStopPosting}
 					/>
 				</Modal>
 			)}
 			<ul className={classes.posts}>
-				<Post author={enteredAuthor} body={enteredBody} />
 				<Post author="Manuel" body="Check out the full course!" />
 			</ul>
 		</>
