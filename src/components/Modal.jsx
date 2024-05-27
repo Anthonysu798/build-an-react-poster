1262f4f3f331ca5a0f6abecf6d9de5929e9f2d73
Anthony Su <anthonysu798.ca@gmail.com>
2024-05-27 02:01:36 -0400

    Fixed Modal backdrop

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import classes from '@/src/components/Modal.module.css';
 
-function Modal({ children, onClose }) { 
+function Modal({ children }) { 
+    const navigate = useNavigate(); 
+
+    // Close the modal when the backdrop is clicked
+    function closeHandler() {
+        navigate('..'); // Navigate up one level (to the parent route)
+    }
+
     return (
         <>
-            <div className={classes.backdrop} onClick={onClose} />
+            <div className={classes.backdrop} onClick={closeHandler} />
             <dialog open={true} className={classes.modal}>{children}</dialog>
         </> 
     );
